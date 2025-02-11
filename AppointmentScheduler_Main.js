e2de0f2af40b35eaa6f3d1466264320c6e9db449
Meet-Rajani <d24it142@charusat.edu.in>
2025-02-11 21:10:50 +0530

    Update AppointmentScheduler_Main.js

--- AppointmentScheduler_Main.js
+++ AppointmentScheduler_Main.js
@@ -1,8 +1,8 @@
 import { addAppointment, getUpcomingAppointments, scheduleReminders } from "./AppointmentScheduler.js";
 
-addAppointment("Yash Bhalodiya", new Date(Date.now() + 30 * 60000), "Consultation");
-addAppointment("Dev Bhalodiya", new Date(Date.now() + 90 * 60000), "Follow-up");
-addAppointment("Jay Parmar", new Date(Date.now() + 45 * 60000), "Check-up");
+addAppointment("Meet Rajani", new Date(Date.now() + 30 * 60000), "Consultation");
+addAppointment("Dhaval Ahir", new Date(Date.now() + 90 * 60000), "Follow-up");
+addAppointment("Yash Bhalodiya", new Date(Date.now() + 45 * 60000), "Check-up");
 
 let upcoming = getUpcomingAppointments();
 console.log("Upcoming Appointments:");
