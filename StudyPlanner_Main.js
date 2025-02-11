c6faa32e60688d2fd13a444f4b5c61e2f5c53c17
Meet-Rajani <d24it142@charusat.edu.in>
2025-02-11 21:12:14 +0530

    Update StudyPlanner_Main.js

--- StudyPlanner_Main.js
+++ StudyPlanner_Main.js
@@ -1,8 +1,8 @@
 import { addSession, listTodaysSessions, sessionCountdown, fetchStudyMaterials } from "./StudyPlanner.js";
 
 addSession("Math", new Date(), 10);
-addSession("Science", new Date(Date.now() + 2 * 60 * 60 * 1000), 15);
-addSession("History", new Date(Date.now() + 24 * 60 * 60 * 1000), 25);
+addSession("DSA", new Date(Date.now() + 2 * 60 * 60 * 1000), 15);
+addSession("Android", new Date(Date.now() + 24 * 60 * 60 * 1000), 25);
 
 console.log("Today's Study Sessions:");
 console.log(listTodaysSessions());
